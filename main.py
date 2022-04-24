94354e3f3f26ce209d861c3cbacefedaf022e829
Jelister <reshetniak.michael@gmail.com>
2022-04-24 19:43:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,13 @@ class Login(QWidget):
 		QWidget.__init__(self)
 		layout = QGridLayout()
 		self.setWindowTitle('Authorization')
+		self.setMaximumSize(QtCore.QSize(360, 640))
+		self.setMinimumSize(QtCore.QSize(360, 640))
 		self.dbname_label = QLabel('Database name:')
+		#self.dbname_label.setStyleSheet("""
+		#	font-size: 18px
+		#	""")
+		#self.dbname_label.move(30,30)
 		layout.addWidget(self.dbname_label)
 		self.dbname_line = QLineEdit()
 		layout.addWidget(self.dbname_line)
@@ -70,18 +76,31 @@ class Login(QWidget):
 class Auth(QWidget):
 	back = QtCore.pyqtSignal()
 	err = QtCore.pyqtSignal(str)
+	conn1 = QtCore.pyqtSignal(str, str, str, str, str)
 	def __init__(self, t1, t2, t3, t4):
 		QWidget.__init__(self)
 		layout = QGridLayout()
 		self.setWindowTitle(str(t1))
+		self.setMaximumSize(QtCore.QSize(800, 600))
+		self.setMinimumSize(QtCore.QSize(800, 600))
 		self.setLayout(layout)
-		label = QLabel('Database name: '+str(t1)+'; User login: '+str(t2)+'; User password: '+str(t3)+'; Database port: '+str(t4))
-		layout.addWidget(label, 1, 0)
+		self.selecter = QLineEdit()
+		layout.addWidget(self.selecter, 0, 0)
+		#label = QLabel('Database name: '+str(t1)+'; User login: '+str(t2)+'; User password: '+str(t3)+'; Database port: '+str(t4))
+		#layout.addWidget(label, 1, 0)
 		self.back_button = QPushButton('Back')
-		self.back_button.clicked.connect(self.backb)
-		layout.addWidget(self.back_button, 0, 0)
+		self.back_button.clicked.connect(self.back)
+		layout.addWidget(self.back_button, 1, 1)
+		self.conn_button = QPushButton('Connect')
+		self.conn_button.clicked.connect(lambda: self.conn(t1, t2, t3, t4))
+		layout.addWidget(self.conn_button, 1, 0)
 	def backb(self):
 		self.back.emit()
+
+	def conn(self, t1, t2, t3, t4):
+		s = 'SELECT * FROM '+self.selecter.text()
+		print(s)
+		self.conn1.emit(s, t1, t2, t3, t4)
 
 class LoginError(QWidget):
 	cl = QtCore.pyqtSignal()
@@ -98,6 +117,22 @@ class LoginError(QWidget):
 	def clact(self):
 		self.cl.emit()
 
+class DataBaseEditor(QWidget):
+	back = QtCore.pyqtSignal()
+	comm = QtCore.pyqtSignal()#add smtn
+	def __init__(self, s, t1, t2, t3, t4):
+		conn = psycopg2.connect(dbname=t1, user=t2, password=t3, port=t4)
+		cur_sql = conn.cursor()
+		cur_sql.execute(str(s))
+		db = cur_sql.fetchall()
+		QWidget.__init__(self)
+		layout = QGridLayout()
+		self.setWindowTitle(s[13:-1]+s[-1])
+		self.setMaximumSize(QtCore.QSize(800, 600))
+		self.setMinimumSize(QtCore.QSize(800, 600))
+		self.label = QLabel(str(db))
+		layout.addWidget(self.label)
+		self.setLayout(layout)
 class Controller:
     def __init__(self):
         pass
@@ -124,17 +159,22 @@ class Controller:
     		self.login.close()
     		self.auth.back.connect(self.show_login)
     		self.auth.err.connect(self.show_err)
+    		self.auth.conn1.connect(self.show_db)
     		self.auth.show()
-    	except Exception as e:
-    		self.show_err(str(e))
+    	except Exception as t:
+    		self.show_err(str(t))
         
-
+    def show_db(self, s, t1, t2, t3, t4):
+    	self.db = DataBaseEditor(s, t1, t2, t3, t4)
+    	self.auth.close()
+    	self.db.show()
     def show_reg(self):
     	self.login.close()
     def hide_err(self):
     	self.erroring.close()
 
     def show_err(self, t):
+    	print(t)
     	self.erroring = LoginError(t)
     	self.erroring.cl.connect(self.hide_err)
     	self.erroring.show()
